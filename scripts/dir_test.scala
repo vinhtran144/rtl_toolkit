c9f0a874c52bd11633ca6d72431df405b29215ab
Vinh Tran <tranvinh144@gmail.com>
2026-09-07 10:14:26 -0400

    Flesh out new template structure for file tree directory

--- scripts/dir_test.scala
+++ scripts/dir_test.scala
@@ -2,7 +2,7 @@
 //> using dep com.lihaoyi::os-lib:0.11.8
 
 
-@main def sv_gen(args: String*): Unit = {
+@main def dirTest(args: String*): Unit = {
     val targetPathStr = if (args.nonEmpty) args(0) else "."
     println("Hello World")
     val currentDir = os.pwd
